c43aa38c9432779c54af802749723e8329eaaae4
Asilbek Ismatilloev <kodniy@gmail.com>
2024-11-29 00:58:54 -0500

    Cleaned comments

--- src/main/java/com/example/project_csc171/HelloApplication.java
+++ src/main/java/com/example/project_csc171/HelloApplication.java
@@ -3,23 +3,17 @@ package com.example.project_csc171;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyCombination;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-// for images
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
-
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-          Scene scene = new Scene(fxmlLoader.load());
+
+        Scene scene = new Scene(fxmlLoader.load());
 
         scene.getRoot().requestFocus();
         stage.setTitle("Hello!");
